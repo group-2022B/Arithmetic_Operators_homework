51239a1b24facb0051dd8f72557fef50a4bad58e
Umidjon <mrumidjon2020@gmail.com>
2022-03-12 17:58:41 +0500

    Arithmetic_Operators_homework

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,5 +1,10 @@
-#Create a variable called 'number' and assign it the two-digit number
-
-#Find the reverse of the number and assign it to a variable called 'answer'.
-
-#Print the answer variable
+# Create a variable called 'number' and assign it the two-digit number
+number = 34
+# Find the reverse of the number and assign it to a variable called 'answer'.
+x1 = number % 10
+number //= 10
+x2 = number % 10
+number //= 10
+answer = x1 * 10 + x2
+# Print the answer variable
+print(answer)
