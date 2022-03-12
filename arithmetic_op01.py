2c7eac25434e74784536ef82b9de5f6cc1c746a9
Umidjon <mrumidjon2020@gmail.com>
2022-03-12 17:54:14 +0500

    assd

--- arithmetic_op01.py
+++ arithmetic_op01.py
@@ -1,5 +1,5 @@
 #Create a variable x and assign it the value of 10.
-x = 10
+x = 1009093938348
 #Create a variable y and assign it the value of 5.
 
 #Create a variable result and assign it the value of x + y.
