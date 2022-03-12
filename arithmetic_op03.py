00deddca570d94885fd60054a059e6de4ae1d949
Umidjon <mrumidjon2020@gmail.com>
2022-03-12 18:02:07 +0500

    answer

--- arithmetic_op03.py
+++ arithmetic_op03.py
@@ -1,10 +1,6 @@
 # Create a variable called 'number' and assign it the two-digit number
 number = 34
 # Find the reverse of the number and assign it to a variable called 'answer'.
-x1 = number % 10
-number //= 10
-x2 = number % 10
-number //= 10
-answer = x1 * 10 + x2
+answer = number // 10 + number % 10 * 10
 # Print the answer variable
 print(answer)
